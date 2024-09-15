20ecc94dfa28a301679b20fed048e0abb0d56b89
Siba Prasad Behera <sibaprasadbehera281@gmail.com>
2024-09-15 21:41:44 +0530

    Add files via upload

---
+++ BookDetailsResponse.java
@@ -0,0 +1,29 @@
+package org.jt.book_store.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.jt.book_store.model.BookFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class BookDetailsResponse {
+        private int bookId;
+        private String bookName;
+        private String authorName;
+        private int totalPage;
+        private String category;
+        private LocalDate publicationDate;
+        private BookFormat bookFormat;
+        private List<String> availability;
+        private String description;
+
+}
